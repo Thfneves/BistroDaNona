481fb19f3ef99043aa414e425c046c4b70b4988d
Thiago Neves <thiago_neves1997@hotmail.com>
2025-01-26 17:06:58 -0300

    no message

---
+++ BistroDaNona/Pages/Home/Cell/StarterDish/StarterDishCollectionViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  StarterDishCollectionViewCell.swift
+//  BistroDaNona
+//
+//  Created by Thiago Neves on 17/01/25.
+//
+
+import UIKit
+
+class StarterDishCollectionViewCell: UICollectionViewCell {
+
+    
+    @IBOutlet weak var titleStarterDish: UILabel!
+    @IBOutlet weak var pictureStarterDish: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+    }
+    
+    static let identifier: String = String(describing: StarterDishCollectionViewCell.self)
+    
+        static func nib() -> UINib {
+        return UINib(nibName: identifier, bundle: nil)
+    }
+
+    
+    func setupCell(whit item: Item){
+        titleStarterDish.text = item.item
+        pictureStarterDish.image = UIImage(named:item.item)
+    }
+    
+}
